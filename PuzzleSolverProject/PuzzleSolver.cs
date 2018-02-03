b1eff6b4425af4eedfb14b17b86d67e1f96ef222
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-02 22:46:35 -0600

    GREEN Throw exception if y position is negative

--- PuzzleSolverProject/PuzzleSolver.cs
+++ PuzzleSolverProject/PuzzleSolver.cs
@@ -26,6 +26,10 @@ namespace PuzzleSolverProject
             {
                 throw new ArgumentOutOfRangeException();
             }
+            else if(y < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
         }
     }
 }
