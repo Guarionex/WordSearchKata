6edd8bf4aac205e7f60c820877ae2c16c6fcc79d
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-02 18:57:11 -0600

    First commit, RED Testing passing string to AddWord method of PuzzleSolver

---
+++ PuzzleSolverUnitTest/PuzzleSolverTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using PuzzleSolverProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleSolverUnitTest
+{
+    [TestFixture]
+    public class PuzzleSolverTest
+    {
+        private PuzzleSolver sut;
+
+        [SetUp]
+        public void init()
+        {
+            sut = new PuzzleSolver();
+        }
+
+        [Test]
+        public void GivenPuzzleSolverWhenPassedALettersOnlyStringThenAddWordThrowsNoException()
+        {
+            sut.AddWord("BONES");
+        }
+    }
+}
