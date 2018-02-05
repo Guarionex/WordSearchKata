60a9a55326fb4cf496b48bba1e8ae81c84af3371
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-05 02:41:54 -0600

    GREEN Made test pass by throwing exception when passed a space in Get2DLetterArray

--- PuzzleSolverProject/PuzzleSolver.cs
+++ PuzzleSolverProject/PuzzleSolver.cs
@@ -111,6 +111,10 @@ namespace PuzzleSolverProject
                 {
                     throw new FormatException();
                 }
+                else if(joinedRow.Any(char.IsWhiteSpace))
+                {
+                    throw new FormatException();
+                }
                 for(int col = 0; col < joinedRow.Length; col++)
                 {
                     letters2dArray[col, row] = joinedRow[col];
