b2d393218a2e8449a1f99b48bf4de077d9178648
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-06 16:02:45 -0600

    RED Made WordSearchPuzzle class and a new test for it.

---
+++ PuzzleSolverUnitTest/WordSearchPuzzleTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using PuzzleSolverProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleSolverUnitTest
+{
+    [TestFixture]
+    public class WordSearchPuzzleTest
+    {
+        private WordSearchPuzzle sut;
+        private PuzzleSolver solver;
+
+        [SetUp]
+        public void init()
+        {
+            sut = new WordSearchPuzzle();
+            solver = new PuzzleSolver();
+        }
+
+        [Test]
+        public void GivenWordSearchPuzzleWhenAddWordIsPassedAStringThenAddWordThrowsNoException()
+        {
+            sut.AddWord("PILLAR");
+        }
+    }
+}
