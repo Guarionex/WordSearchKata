dd77218df03793f5daa37678ea1c168519d8ea21
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-06 16:21:09 -0600

    GREEN Making test pass by saving words added to a list in AddWord and returning them in GetWords

--- PuzzleSolverProject/WordSearchPuzzle.cs
+++ PuzzleSolverProject/WordSearchPuzzle.cs
@@ -8,17 +8,15 @@ namespace PuzzleSolverProject
 {
     public class WordSearchPuzzle
     {
+        private List<String> words = new List<string>();
         public void AddWord(String word)
         {
-            
+            words.Add(word);
         }
 
         public List<String> GetWords()
         {
-            List<String> expected = new List<String>();
-            expected.Add("PILLAR");
-
-            return expected;
+            return words;
         }
     }
 }
