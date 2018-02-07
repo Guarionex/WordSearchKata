62d75d010fe9a1352656e45a1ff814a1c61e9071
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-06 23:05:47 -0600

    RED Test for diferent UpRight set of positions

--- PuzzleSolverUnitTest/WordSearchPuzzleTest.cs
+++ PuzzleSolverUnitTest/WordSearchPuzzleTest.cs
@@ -437,5 +437,34 @@ namespace PuzzleSolverUnitTest
             Assert.AreEqual(expected, result);
         }
 
+        [Test]
+        public void Given4x4WordPuzzleWhenCallingGetUpRightNeighboorsOfByWith02By3ThenGetUpRightNeighboorsOfByReturnsAListOfVector2WithUpRightPositionsFromBottomLeftToTopRight()
+        {
+            sut.AddLetterAt('K', 0, 0);
+            sut.AddLetterAt('G', 1, 0);
+            sut.AddLetterAt('J', 2, 0);
+            sut.AddLetterAt('K', 3, 0);
+            sut.AddLetterAt('I', 0, 1);
+            sut.AddLetterAt('U', 1, 1);
+            sut.AddLetterAt('R', 2, 1);
+            sut.AddLetterAt('F', 3, 1);
+            sut.AddLetterAt('R', 0, 2);
+            sut.AddLetterAt('I', 1, 2);
+            sut.AddLetterAt('G', 2, 2);
+            sut.AddLetterAt('J', 3, 2);
+            sut.AddLetterAt('K', 0, 3);
+            sut.AddLetterAt('H', 1, 3);
+            sut.AddLetterAt('A', 2, 3);
+            sut.AddLetterAt('N', 3, 3);
+
+            Vector2 startPosition = new Vector2(0, 2);
+            List<Vector2> result = sut.GetUpRightNeighborsOfBy(startPosition, 3);
+            List<Vector2> expected = new List<Vector2>();
+            expected.Add(new Vector2(0, 2));
+            expected.Add(new Vector2(1, 1));
+            expected.Add(new Vector2(2, 0));
+
+            Assert.AreEqual(expected, result);
+        }
     }
 }
