1f97c187c0e1365363a8f092af435e2de881facc
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-06 18:25:51 -0600

    RED Test for value of FindAllLetterPositions

--- PuzzleSolverUnitTest/WordSearchPuzzleTest.cs
+++ PuzzleSolverUnitTest/WordSearchPuzzleTest.cs
@@ -182,5 +182,32 @@ namespace PuzzleSolverUnitTest
             Assert.IsInstanceOf(typeof(List<Vector2>), sut.FindAllLetterPositions('K'));
         }
 
+        [Test]
+        public void GivenALetterWhenPassedToFindAllLetterPositionsThenFinAllLetterPositionsReturnsAListOfVector2WithValidPositionsForTheLetter()
+        {
+            sut.AddLetterAt('Q', 0, 0);
+            sut.AddLetterAt('G', 1, 0);
+            sut.AddLetterAt('J', 2, 0);
+            sut.AddLetterAt('N', 3, 0);
+            sut.AddLetterAt('X', 0, 1);
+            sut.AddLetterAt('U', 1, 1);
+            sut.AddLetterAt('A', 2, 1);
+            sut.AddLetterAt('F', 3, 1);
+            sut.AddLetterAt('L', 0, 2);
+            sut.AddLetterAt('H', 1, 2);
+            sut.AddLetterAt('G', 2, 2);
+            sut.AddLetterAt('J', 3, 2);
+            sut.AddLetterAt('K', 0, 3);
+            sut.AddLetterAt('I', 1, 3);
+            sut.AddLetterAt('R', 2, 3);
+            sut.AddLetterAt('K', 3, 3);
+
+            List<Vector2> result = sut.FindAllLetterPositions('K');
+            List<Vector2> expected = new List<Vector2>();
+            expected.Add(new Vector2(0, 3));
+            expected.Add(new Vector2(3, 3));
+
+            Assert.AreEqual(expected, result);
+        }
     }
 }
