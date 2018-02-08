18cfdb075e2c601696dbe23958e681a2b4f90284
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-08 00:44:04 -0600

    REFACTOR Moving common algorithm between caller method and predicates

--- PuzzleSolverProject/WordSearchPuzzle.cs
+++ PuzzleSolverProject/WordSearchPuzzle.cs
@@ -79,28 +79,6 @@ namespace PuzzleSolverProject
             return SearchWordInDirection(word, DirectionEnum.DownRight);
         }
 
-        private List<Vector2> GetRightNeighborsStartingFrom(Vector2 startPosition, int length)
-        {
-            Vector2 maxPosition = new Vector2(startPosition.X + length - 1, startPosition.Y);
-            List<Vector2> positionsWithinRange = Letters.Select(kvp => kvp.Key).Where(key => (maxPosition - key).X >= 0 && (maxPosition - key).Y == 0).ToList();
-            List<Vector2> positionsRightOfStartingPoint = positionsWithinRange.Where(vector => vector.X >= startPosition.X).ToList();
-           return positionsRightOfStartingPoint;
-        }
-
-        private List<Vector2> FindAllLetterPositions(Char letter)
-        {
-            List<Vector2> positions = Letters.Where(kvp => kvp.Value == letter).Select(kvp => kvp.Key).ToList();
-            return positions;
-        }
-
-        private List<Vector2> GetDownNeighborsStartingFrom(Vector2 startPosition, int length)
-        {
-            Vector2 maxPosition = new Vector2(startPosition.X, startPosition.Y + length - 1);
-            List<Vector2> positionsWithinRange = Letters.Select(kvp => kvp.Key).Where(key => (maxPosition - key).X == 0 && (maxPosition - key).Y >= 0).ToList();
-            List<Vector2> positionsBottomOfStartPosition = positionsWithinRange.Where(vector => vector.Y >= startPosition.Y).ToList();
-            return positionsBottomOfStartPosition;
-        }
-
         private List<Vector2> SearchWordInDirection(String word, DirectionEnum direction)
         {
             List<Vector2> wordPosition = new List<Vector2>();
@@ -119,7 +97,29 @@ namespace PuzzleSolverProject
 
             return wordPosition;
         }
-        
+
+        private List<Vector2> FindAllLetterPositions(Char letter)
+        {
+            List<Vector2> positions = Letters.Where(kvp => kvp.Value == letter).Select(kvp => kvp.Key).ToList();
+            return positions;
+        }
+
+        private List<Vector2> GetRightNeighborsStartingFrom(Vector2 startPosition, int length)
+        {
+            Vector2 maxPosition = new Vector2(startPosition.X + length - 1, startPosition.Y);
+            List<Vector2> positionsWithinRange = Letters.Select(kvp => kvp.Key).Where(key => (maxPosition - key).X >= 0 && (maxPosition - key).Y == 0).ToList();
+            List<Vector2> positionsRightOfStartingPoint = positionsWithinRange.Where(vector => vector.X >= startPosition.X).ToList();
+           return positionsRightOfStartingPoint;
+        }
+
+        private List<Vector2> GetDownNeighborsStartingFrom(Vector2 startPosition, int length)
+        {
+            Vector2 maxPosition = new Vector2(startPosition.X, startPosition.Y + length - 1);
+            List<Vector2> positionsWithinRange = Letters.Select(kvp => kvp.Key).Where(key => (maxPosition - key).X == 0 && (maxPosition - key).Y >= 0).ToList();
+            List<Vector2> positionsBottomOfStartPosition = positionsWithinRange.Where(vector => vector.Y >= startPosition.Y).ToList();
+            return positionsBottomOfStartPosition;
+        }
+
         private List<Vector2> GetUpRightNeighborsStartingFrom(Vector2 startPosition, int length)
         {
             List<Vector2> positionsUpRightFromStartPosition = new List<Vector2>();
