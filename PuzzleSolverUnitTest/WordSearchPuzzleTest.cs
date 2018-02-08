9b9ed805124d69c24da861adba5969cf4697d7d1
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-08 13:32:01 -0600

    RED Made passing test for two down words. Made passing test for an up word and down word. Made failing test for a left word

--- PuzzleSolverUnitTest/WordSearchPuzzleTest.cs
+++ PuzzleSolverUnitTest/WordSearchPuzzleTest.cs
@@ -1058,5 +1058,122 @@ namespace PuzzleSolverUnitTest
 
             Assert.AreEqual(expected, result);
         }
+
+        [Test]
+        public void Given4x4WordWithTwoDownWordsPuzzleWhenCallingGetWordsLocationsThenGetWordsReturnsAStringVector2DictionaryWithEntryForAllWords()
+        {
+            sut.AddWord("KIRK");
+            sut.AddWord("KHAN");
+            sut.AddLetterAt('K', 0, 0);
+            sut.AddLetterAt('E', 1, 0);
+            sut.AddLetterAt('F', 2, 0);
+            sut.AddLetterAt('K', 3, 0);
+            sut.AddLetterAt('I', 0, 1);
+            sut.AddLetterAt('R', 1, 1);
+            sut.AddLetterAt('J', 2, 1);
+            sut.AddLetterAt('H', 3, 1);
+            sut.AddLetterAt('R', 0, 2);
+            sut.AddLetterAt('L', 1, 2);
+            sut.AddLetterAt('I', 2, 2);
+            sut.AddLetterAt('A', 3, 2);
+            sut.AddLetterAt('K', 0, 3);
+            sut.AddLetterAt('D', 1, 3);
+            sut.AddLetterAt('J', 2, 3);
+            sut.AddLetterAt('N', 3, 3);
+
+            Dictionary<String, List<Vector2>> result = sut.GetWordsLocation();
+            List<Vector2> kirkLocation = new List<Vector2>();
+            kirkLocation.Add(new Vector2(0, 0));
+            kirkLocation.Add(new Vector2(0, 1));
+            kirkLocation.Add(new Vector2(0, 2));
+            kirkLocation.Add(new Vector2(0, 3));
+
+            List<Vector2> khanLocation = new List<Vector2>();
+            khanLocation.Add(new Vector2(3, 0));
+            khanLocation.Add(new Vector2(3, 1));
+            khanLocation.Add(new Vector2(3, 2));
+            khanLocation.Add(new Vector2(3, 3));
+
+            Dictionary<String, List<Vector2>> expected = new Dictionary<String, List<Vector2>>();
+            expected.Add("KIRK", kirkLocation);
+            expected.Add("KHAN", khanLocation);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Given4x4WordWithUpAndDownWordsPuzzleWhenCallingGetWordsLocationsThenGetWordsReturnsAStringVector2DictionaryWithEntryForAllWords()
+        {
+            sut.AddWord("KIRK");
+            sut.AddWord("KHAN");
+            sut.AddLetterAt('K', 0, 0);
+            sut.AddLetterAt('E', 1, 0);
+            sut.AddLetterAt('F', 2, 0);
+            sut.AddLetterAt('N', 3, 0);
+            sut.AddLetterAt('I', 0, 1);
+            sut.AddLetterAt('R', 1, 1);
+            sut.AddLetterAt('J', 2, 1);
+            sut.AddLetterAt('A', 3, 1);
+            sut.AddLetterAt('R', 0, 2);
+            sut.AddLetterAt('L', 1, 2);
+            sut.AddLetterAt('I', 2, 2);
+            sut.AddLetterAt('H', 3, 2);
+            sut.AddLetterAt('K', 0, 3);
+            sut.AddLetterAt('D', 1, 3);
+            sut.AddLetterAt('J', 2, 3);
+            sut.AddLetterAt('K', 3, 3);
+
+            Dictionary<String, List<Vector2>> result = sut.GetWordsLocation();
+            List<Vector2> kirkLocation = new List<Vector2>();
+            kirkLocation.Add(new Vector2(0, 0));
+            kirkLocation.Add(new Vector2(0, 1));
+            kirkLocation.Add(new Vector2(0, 2));
+            kirkLocation.Add(new Vector2(0, 3));
+
+            List<Vector2> khanLocation = new List<Vector2>();
+            khanLocation.Add(new Vector2(3, 3));
+            khanLocation.Add(new Vector2(3, 2));
+            khanLocation.Add(new Vector2(3, 1));
+            khanLocation.Add(new Vector2(3, 0));
+
+            Dictionary<String, List<Vector2>> expected = new Dictionary<String, List<Vector2>>();
+            expected.Add("KIRK", kirkLocation);
+            expected.Add("KHAN", khanLocation);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Given4x4WordWithOneLeftWordsPuzzleWhenCallingGetWordsLocationsThenGetWordsReturnsAStringVector2DictionaryWithEntryForKIRK()
+        {
+            sut.AddWord("KIRK");
+            sut.AddLetterAt('K', 0, 0);
+            sut.AddLetterAt('E', 1, 0);
+            sut.AddLetterAt('F', 2, 0);
+            sut.AddLetterAt('N', 3, 0);
+            sut.AddLetterAt('K', 0, 1);
+            sut.AddLetterAt('R', 1, 1);
+            sut.AddLetterAt('I', 2, 1);
+            sut.AddLetterAt('K', 3, 1);
+            sut.AddLetterAt('R', 0, 2);
+            sut.AddLetterAt('L', 1, 2);
+            sut.AddLetterAt('I', 2, 2);
+            sut.AddLetterAt('H', 3, 2);
+            sut.AddLetterAt('K', 0, 3);
+            sut.AddLetterAt('D', 1, 3);
+            sut.AddLetterAt('J', 2, 3);
+            sut.AddLetterAt('M', 3, 3);
+
+            Dictionary<String, List<Vector2>> result = sut.GetWordsLocation();
+            List<Vector2> kirkLocation = new List<Vector2>();
+            kirkLocation.Add(new Vector2(3, 1));
+            kirkLocation.Add(new Vector2(2, 1));
+            kirkLocation.Add(new Vector2(1, 1));
+            kirkLocation.Add(new Vector2(0, 1));
+            Dictionary<String, List<Vector2>> expected = new Dictionary<String, List<Vector2>>();
+            expected.Add("KIRK", kirkLocation);
+
+            Assert.AreEqual(expected, result);
+        }
     }
 }
