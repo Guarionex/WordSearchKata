86ccb572d5d9ca080db7df01d22a01329ec38bf9
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-09 13:52:54 -0600

    RED Making test for toString on an invalid puzzle

--- PuzzleSolverUnitTest/WordSearchPuzzleTest.cs
+++ PuzzleSolverUnitTest/WordSearchPuzzleTest.cs
@@ -1093,5 +1093,34 @@ namespace PuzzleSolverUnitTest
 
             Assert.AreEqual(expected, result);
         }
+
+        [Test]
+        public void Given4x4WordPuzzleWithAWordMissingWhenCallingToStringThenToStringReturnsAnEmptyString()
+        {
+            sut.AddWord("KIRK");
+            sut.AddWord("KHAN");
+            sut.AddWord("SULU");
+            sut.AddLetterAt('S', 0, 0);
+            sut.AddLetterAt('X', 1, 0);
+            sut.AddLetterAt('L', 2, 0);
+            sut.AddLetterAt('U', 3, 0);
+            sut.AddLetterAt('K', 0, 1);
+            sut.AddLetterAt('I', 1, 1);
+            sut.AddLetterAt('R', 2, 1);
+            sut.AddLetterAt('K', 3, 1);
+            sut.AddLetterAt('R', 0, 2);
+            sut.AddLetterAt('L', 1, 2);
+            sut.AddLetterAt('I', 2, 2);
+            sut.AddLetterAt('H', 3, 2);
+            sut.AddLetterAt('K', 0, 3);
+            sut.AddLetterAt('H', 1, 3);
+            sut.AddLetterAt('A', 2, 3);
+            sut.AddLetterAt('N', 3, 3);
+
+            String result = sut.ToString();
+            String expected = "";
+
+            Assert.AreEqual(expected, result);
+        }
     }
 }
