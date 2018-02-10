8cebce1106c71180256f500ce7bc8018f89b9268
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-09 19:41:26 -0600

    REFACTOR Interfacing out the direction search predicates. Test coverage will be avaiable in GetWordsLocation, IsValid, and toString once it gets hooked up

---
+++ PuzzleSolverProject/IGetNeighborsFrom.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleSolverProject
+{
+    interface IGetNeighborsFrom
+    {
+        void AddPositionToLettersDictionary(Dictionary<Vector2, Char> positionsToLetters);
+        List<Vector2> GetNeighborsFrom(Vector2 startPosition, int length);
+    }
+}
