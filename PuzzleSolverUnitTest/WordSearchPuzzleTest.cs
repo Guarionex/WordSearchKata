e0ac12881742886740b86107d9520ffe13cdc138
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-11 20:32:45 -0600

    REFACTOR Helper setup for two left words

--- PuzzleSolverUnitTest/WordSearchPuzzleTest.cs
+++ PuzzleSolverUnitTest/WordSearchPuzzleTest.cs
@@ -21,6 +21,7 @@ namespace PuzzleSolverUnitTest
         private const String TwoDownWordTestCase = nameof(WordSearchPuzzleTestData.TwoDownWordTestCase);
         private const String OneUpOneDownWordTestCase = nameof(WordSearchPuzzleTestData.OneUpOneDownWordTestCase);
         private const String OneLeftWordTestCase = nameof(WordSearchPuzzleTestData.OneLeftWordTestCase);
+        private const String TwoLeftWordTestCase = nameof(WordSearchPuzzleTestData.TwoLeftWordTestCase);
 
         private WordSearchPuzzle sut;
 
@@ -181,43 +182,11 @@ namespace PuzzleSolverUnitTest
             Assert.AreEqual(expected, result);
         }
 
-        [Test]
-        public void Given4x4WordWithTwoLeftWordsPuzzleWhenCallingGetWordsLocationsThenGetWordsReturnsAStringVector2DictionaryWithEntryForAllWords()
+        [Test, TestCaseSource(typeof(WordSearchPuzzleTestData), OneLeftWordTestCase)]
+        public void Given4x4WordWithTwoLeftWordsPuzzleWhenCallingGetWordsLocationsThenGetWordsReturnsAStringVector2DictionaryWithEntryForAllWords(WordSearchPuzzle setupSUT, Dictionary<String, List<Vector2>> expected)
         {
-            sut.AddWord("KIRK");
-            sut.AddWord("KHAN");
-            sut.AddLetterAt('K', 0, 0);
-            sut.AddLetterAt('E', 1, 0);
-            sut.AddLetterAt('F', 2, 0);
-            sut.AddLetterAt('N', 3, 0);
-            sut.AddLetterAt('K', 0, 1);
-            sut.AddLetterAt('R', 1, 1);
-            sut.AddLetterAt('I', 2, 1);
-            sut.AddLetterAt('K', 3, 1);
-            sut.AddLetterAt('R', 0, 2);
-            sut.AddLetterAt('L', 1, 2);
-            sut.AddLetterAt('X', 2, 2);
-            sut.AddLetterAt('H', 3, 2);
-            sut.AddLetterAt('N', 0, 3);
-            sut.AddLetterAt('A', 1, 3);
-            sut.AddLetterAt('H', 2, 3);
-            sut.AddLetterAt('K', 3, 3);
-
+            sut = setupSUT;
             Dictionary<String, List<Vector2>> result = sut.GetWordsLocation();
-            List<Vector2> kirkLocation = new List<Vector2>();
-            kirkLocation.Add(new Vector2(3, 1));
-            kirkLocation.Add(new Vector2(2, 1));
-            kirkLocation.Add(new Vector2(1, 1));
-            kirkLocation.Add(new Vector2(0, 1));
-
-            List<Vector2> khanLocation = new List<Vector2>();
-            khanLocation.Add(new Vector2(3, 3));
-            khanLocation.Add(new Vector2(2, 3));
-            khanLocation.Add(new Vector2(1, 3));
-            khanLocation.Add(new Vector2(0, 3));
-            Dictionary<String, List<Vector2>> expected = new Dictionary<String, List<Vector2>>();
-            expected.Add("KIRK", kirkLocation);
-            expected.Add("KHAN", khanLocation);
 
             Assert.AreEqual(expected, result);
         }
