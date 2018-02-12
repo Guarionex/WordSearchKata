4eebac4743234eecf19bda4adf18a6a630068d31
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-11 19:07:33 -0600

    REFACTOR Helper setup method for two down words

--- PuzzleSolverUnitTest/WordSearchPuzzleTest.cs
+++ PuzzleSolverUnitTest/WordSearchPuzzleTest.cs
@@ -18,6 +18,7 @@ namespace PuzzleSolverUnitTest
         private const String OneOfTwoWordsMissingInPuzzleTestCase = nameof(WordSearchPuzzleTestData.OneOfTwoWordsMissingInPuzzleTestCase);
         private const String TwoWordsUpTestCase = nameof(WordSearchPuzzleTestData.TwoWordsUpTestCase);
         private const String OneDownWordTestCase = nameof(WordSearchPuzzleTestData.OneDownWordTestCase);
+        private const String TwoDownWordTestCase = nameof(WordSearchPuzzleTestData.TwoDownWordTestCase);
 
         private WordSearchPuzzle sut;
 
@@ -151,44 +152,11 @@ namespace PuzzleSolverUnitTest
             Assert.AreEqual(expected, result);
         }
 
-        [Test]
-        public void Given4x4WordWithTwoDownWordsPuzzleWhenCallingGetWordsLocationsThenGetWordsReturnsAStringVector2DictionaryWithEntryForAllWords()
+        [Test, TestCaseSource(typeof(WordSearchPuzzleTestData), TwoDownWordTestCase)]
+        public void Given4x4WordWithTwoDownWordsPuzzleWhenCallingGetWordsLocationsThenGetWordsReturnsAStringVector2DictionaryWithEntryForAllWords(WordSearchPuzzle setupSUT, Dictionary<String, List<Vector2>> expected)
         {
-            sut.AddWord("KIRK");
-            sut.AddWord("KHAN");
-            sut.AddLetterAt('K', 0, 0);
-            sut.AddLetterAt('E', 1, 0);
-            sut.AddLetterAt('F', 2, 0);
-            sut.AddLetterAt('K', 3, 0);
-            sut.AddLetterAt('I', 0, 1);
-            sut.AddLetterAt('R', 1, 1);
-            sut.AddLetterAt('J', 2, 1);
-            sut.AddLetterAt('H', 3, 1);
-            sut.AddLetterAt('R', 0, 2);
-            sut.AddLetterAt('L', 1, 2);
-            sut.AddLetterAt('I', 2, 2);
-            sut.AddLetterAt('A', 3, 2);
-            sut.AddLetterAt('K', 0, 3);
-            sut.AddLetterAt('D', 1, 3);
-            sut.AddLetterAt('J', 2, 3);
-            sut.AddLetterAt('N', 3, 3);
-
+            sut = setupSUT;
             Dictionary<String, List<Vector2>> result = sut.GetWordsLocation();
-            List<Vector2> kirkLocation = new List<Vector2>();
-            kirkLocation.Add(new Vector2(0, 0));
-            kirkLocation.Add(new Vector2(0, 1));
-            kirkLocation.Add(new Vector2(0, 2));
-            kirkLocation.Add(new Vector2(0, 3));
-
-            List<Vector2> khanLocation = new List<Vector2>();
-            khanLocation.Add(new Vector2(3, 0));
-            khanLocation.Add(new Vector2(3, 1));
-            khanLocation.Add(new Vector2(3, 2));
-            khanLocation.Add(new Vector2(3, 3));
-
-            Dictionary<String, List<Vector2>> expected = new Dictionary<String, List<Vector2>>();
-            expected.Add("KIRK", kirkLocation);
-            expected.Add("KHAN", khanLocation);
 
             Assert.AreEqual(expected, result);
         }
--- PuzzleSolverUnitTest/WordSearchPuzzleTestData.cs
+++ PuzzleSolverUnitTest/WordSearchPuzzleTestData.cs
@@ -148,6 +148,50 @@ namespace PuzzleSolverUnitTest
             }
         }
 
+        public static IEnumerable TwoDownWordTestCase
+        {
+            get
+            {
+                WordSearchPuzzle setupSUT = new WordSearchPuzzle();
+                setupSUT.AddWord("KIRK");
+                setupSUT.AddWord("KHAN");
+                setupSUT.AddLetterAt('K', 0, 0);
+                setupSUT.AddLetterAt('E', 1, 0);
+                setupSUT.AddLetterAt('F', 2, 0);
+                setupSUT.AddLetterAt('K', 3, 0);
+                setupSUT.AddLetterAt('I', 0, 1);
+                setupSUT.AddLetterAt('R', 1, 1);
+                setupSUT.AddLetterAt('J', 2, 1);
+                setupSUT.AddLetterAt('H', 3, 1);
+                setupSUT.AddLetterAt('R', 0, 2);
+                setupSUT.AddLetterAt('L', 1, 2);
+                setupSUT.AddLetterAt('I', 2, 2);
+                setupSUT.AddLetterAt('A', 3, 2);
+                setupSUT.AddLetterAt('K', 0, 3);
+                setupSUT.AddLetterAt('D', 1, 3);
+                setupSUT.AddLetterAt('J', 2, 3);
+                setupSUT.AddLetterAt('N', 3, 3);
+
+                List<Vector2> kirkLocation = new List<Vector2>();
+                kirkLocation.Add(new Vector2(0, 0));
+                kirkLocation.Add(new Vector2(0, 1));
+                kirkLocation.Add(new Vector2(0, 2));
+                kirkLocation.Add(new Vector2(0, 3));
+
+                List<Vector2> khanLocation = new List<Vector2>();
+                khanLocation.Add(new Vector2(3, 0));
+                khanLocation.Add(new Vector2(3, 1));
+                khanLocation.Add(new Vector2(3, 2));
+                khanLocation.Add(new Vector2(3, 3));
+
+                Dictionary<String, List<Vector2>> expected = new Dictionary<String, List<Vector2>>();
+                expected.Add("KIRK", kirkLocation);
+                expected.Add("KHAN", khanLocation);
+
+                yield return new TestCaseData(setupSUT, expected);
+            }
+        }
+
         private static WordSearchPuzzle KIRKUpInFirstColumnPuzzle()
         {
             WordSearchPuzzle setupSUT = new WordSearchPuzzle();
