2af86d993f9230e02097e4888fd9f6e90d36df4b
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-11 18:30:02 -0600

    REFACTOR helper method for missing word in puzzle

--- PuzzleSolverUnitTest/WordSearchPuzzleTest.cs
+++ PuzzleSolverUnitTest/WordSearchPuzzleTest.cs
@@ -14,6 +14,7 @@ namespace PuzzleSolverUnitTest
     {
         private const String GetWordsLocationReturnTypeTestCase = nameof(WordSearchPuzzleTestData.GetWordsLocationReturnTypeTestCase);
         private const String KIRKUpInFirstColumnTestCase = nameof(WordSearchPuzzleTestData.KIRKUpInFirstColumnTestCase);
+        private const String WordMissingInPuzzleTestCase = nameof(WordSearchPuzzleTestData.WordMissingInPuzzleTestCase);
 
         private WordSearchPuzzle sut;
 
@@ -102,26 +103,10 @@ namespace PuzzleSolverUnitTest
             Assert.IsInstanceOf(typeof(Dictionary<String, List<Vector2>>), sut.GetWordsLocation());
         }
 
-        [Test]
-        public void Given4x4WordPuzzleWithWordNotInPuzzleWhenCallingGetWordsLocationsThenGetWordsLicationsThrowsNoException()
+        [Test, TestCaseSource(typeof(WordSearchPuzzleTestData), WordMissingInPuzzleTestCase)]
+        public void Given4x4WordPuzzleWithWordNotInPuzzleWhenCallingGetWordsLocationsThenGetWordsLicationsThrowsNoException(WordSearchPuzzle setupSUT)
         {
-            sut.AddWord("KIRK");
-            sut.AddLetterAt('X', 0, 0);
-            sut.AddLetterAt('E', 1, 0);
-            sut.AddLetterAt('F', 2, 0);
-            sut.AddLetterAt('N', 3, 0);
-            sut.AddLetterAt('R', 0, 1);
-            sut.AddLetterAt('R', 1, 1);
-            sut.AddLetterAt('J', 2, 1);
-            sut.AddLetterAt('A', 3, 1);
-            sut.AddLetterAt('I', 0, 2);
-            sut.AddLetterAt('L', 1, 2);
-            sut.AddLetterAt('I', 2, 2);
-            sut.AddLetterAt('H', 3, 2);
-            sut.AddLetterAt('K', 0, 3);
-            sut.AddLetterAt('D', 1, 3);
-            sut.AddLetterAt('J', 2, 3);
-            sut.AddLetterAt('M', 3, 3);
+            sut = setupSUT;
             sut.GetWordsLocation();
         }
 
