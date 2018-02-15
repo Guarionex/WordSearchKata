602207f9ebb2ebc0d61a574bc65e5c0fc363f49c
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-14 19:24:24 -0600

    RED Separating method for doing saerch from WordSeachPuzzle and making it's accompanying test

---
+++ PuzzleSolverProject/WordsLocationFinder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleSolverProject
+{
+    public class WordsLocationFinder
+    {
+        public Dictionary<String, List<Vector2>> GetWordsLocation(WordSearchPuzzle puzzle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PuzzleSolverUnitTest/WordsLocationFinderTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using PuzzleSolverProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleSolverUnitTest
+{
+    [TestFixture]
+    public class WordsLocationFinderTest
+    {
+        private const String ValidOneWordPuzzleTestCase = nameof(WordLocationFinderTestData.ValidOneWordPuzzleTestCase);
+
+        private WordsLocationFinder sut;
+
+        [SetUp]
+        public void init()
+        {
+            sut = new WordsLocationFinder();
+        }
+
+        [Test, TestCaseSource(typeof(WordLocationFinderTestData), ValidOneWordPuzzleTestCase)]
+        public void Given4x4WordWithTwoWordsPuzzleWhenCallingGetWordsLocationsThenGetWordsReturnsAStringListVector2Dictionary(WordSearchPuzzle puzzle)
+        {
+            Dictionary<String, List<Vector2>> result = sut.GetWordsLocation(puzzle);
+            Assert.IsInstanceOf(typeof(Dictionary<String, List<Vector2>>), result);
+        }
+    }
+}
