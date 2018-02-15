a0e12964bb03d6e0bb149e4f897fd1999f94bf84
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-15 11:57:46 -0600

    REFACTOR Creating Decorator for Dictionary<String, List<Vector2>> to have IsValid and ToString responsabilities. Creating it's test too

---
+++ PuzzleSolverProject/WordsMapDecorator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleSolverProject
+{
+    public class WordsMapDecorator : Dictionary<String, List<Vector2>>
+    {
+        private WordsLocationFinder wordsFinder;
+
+        public WordsMapDecorator(WordsLocationFinder wordsLocationFinder)
+        {
+            this.wordsFinder = wordsLocationFinder;
+        }
+    }
+}
---
+++ PuzzleSolverUnitTest/WordsMapDecoratorTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using PuzzleSolverProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleSolverUnitTest
+{
+    [TestFixture]
+    public class WordsMapDecoratorTest
+    {
+        private WordsMapDecorator sut;
+        [Test]
+        public void GivenWordsLocationFinderWhenPassToWordsMapDecoratorConstructorThenThrowNoException()
+        {
+            WordSearchPuzzle puzzle = new WordSearchPuzzle();
+            WordsLocationFinder wordsFinder = new WordsLocationFinder(puzzle);
+
+            sut = new WordsMapDecorator(wordsFinder);
+        }
+    }
+}
