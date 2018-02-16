6bb2522fc4757c7a742cb07d7e2ec3d5653ef44f
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-16 00:51:06 -0600

    REFACTOR Making passing testr for value of DownRight GetNeighborsFrom

--- PuzzleSolverUnitTest/DirectionSearchStrategyTests/DownRightDirectionSearchStrategyTest.cs
+++ PuzzleSolverUnitTest/DirectionSearchStrategyTests/DownRightDirectionSearchStrategyTest.cs
@@ -4,6 +4,7 @@ using PuzzleSolverProject.GetNeighbors;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,24 @@ namespace PuzzleSolverUnitTest.DirectionSearchStrategyTests
         protected override IDirectionSearchStrategy CreateInstance(WordSearchPuzzle puzzle)
         {
             return new DownRightDirectionSearchStrategy(puzzle);
+        }
+
+        [Test, TestCaseSource(typeof(DirectionSearchStrategyTestData), Base4x4PuzzleTestCase)]
+        public void Given4x4WordSearchPuzzleWhenPassing00And4ToGetNeighborsFromThenGetNeighborsFromReturnsListOfLocationsFrom00DownRight(WordSearchPuzzle puzzle)
+        {
+            IDirectionSearchStrategy sut = CreateInstance(puzzle);
+
+            Vector2 startLocation = new Vector2(0, 0);
+            int length = 4;
+
+            List<Vector2> result = sut.GetNeighborsFrom(startLocation, length);
+            List<Vector2> expected = new List<Vector2>();
+            expected.Add(new Vector2(0, 0));
+            expected.Add(new Vector2(1, 1));
+            expected.Add(new Vector2(2, 2));
+            expected.Add(new Vector2(3, 3));
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
