24527abdfee5d47fe7032a00933ee282992009c2
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-16 16:16:53 -0600

    REFACTOR Removing unused test data

--- PuzzleSolverUnitTest/WordsLocationFinderTest.cs
+++ PuzzleSolverUnitTest/WordsLocationFinderTest.cs
@@ -17,15 +17,6 @@ namespace PuzzleSolverUnitTest
         private const String PuzzleWithNoWordsAddedTestCase = nameof(WordLocationFinderTestData.PuzzleWithNoWordsAddedTestCase);
         private const String OneUpWordTestCase = nameof(WordLocationFinderTestData.OneUpWordTestCase);
         private const String TwoWordsUpTestCase = nameof(WordLocationFinderTestData.TwoWordsUpTestCase);
-        private const String DownWordTestCase = nameof(WordLocationFinderTestData.DownWordTestCase);
-        private const String OneUpOneDownWordTestCase = nameof(WordLocationFinderTestData.OneUpOneDownWordTestCase);
-        private const String LeftWordTestCase = nameof(WordLocationFinderTestData.LeftWordTestCase);
-        private const String RightWordTestCase = nameof(WordLocationFinderTestData.RightWordTestCase);
-        private const String UpLeftWordTestCase = nameof(WordLocationFinderTestData.UpLeftWordTestCase);
-        private const String UpRightWordTestCase = nameof(WordLocationFinderTestData.UpRightWordTestCase);
-        private const String DownLeftWordTestCase = nameof(WordLocationFinderTestData.DownLeftWordTestCase);
-        private const String DownRightWordTestCase = nameof(WordLocationFinderTestData.DownRightWordTestCase);
-        private const String PuzzleWithWordTwiceTestCase = nameof(WordLocationFinderTestData.PuzzleWithWordTwiceTestCase);
 
         private WordsLocationFinder sut;
 
