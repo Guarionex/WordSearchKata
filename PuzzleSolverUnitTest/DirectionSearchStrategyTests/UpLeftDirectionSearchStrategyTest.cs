76eab8dee0c11642cc97891ff8d8f2aa8747babe
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-16 00:27:51 -0600

    REFACTOR Making passing test for value of UpLeft GetNeighborsFrom

--- PuzzleSolverUnitTest/DirectionSearchStrategyTests/UpLeftDirectionSearchStrategyTest.cs
+++ PuzzleSolverUnitTest/DirectionSearchStrategyTests/UpLeftDirectionSearchStrategyTest.cs
@@ -4,6 +4,7 @@ using PuzzleSolverProject.GetNeighbors;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,24 @@ namespace PuzzleSolverUnitTest.DirectionSearchStrategyTests
         protected override IDirectionSearchStrategy CreateInstance(WordSearchPuzzle puzzle)
         {
             return new UpLeftDirectionSearchStrategy(puzzle);
+        }
+
+        [Test, TestCaseSource(typeof(DirectionSearchStrategyTestData), Base4x4PuzzleTestCase)]
+        public void Given4x4WordSearchPuzzleWhenPassing33And4ToGetNeighborsFromThenGetNeighborsFromReturnsListOfLocationsFrom33UpLeft(WordSearchPuzzle puzzle)
+        {
+            IDirectionSearchStrategy sut = CreateInstance(puzzle);
+
+            Vector2 startLocation = new Vector2(3, 3);
+            int length = 4;
+
+            List<Vector2> result = sut.GetNeighborsFrom(startLocation, length);
+            List<Vector2> expected = new List<Vector2>();
+            expected.Add(new Vector2(3, 3));
+            expected.Add(new Vector2(2, 2));
+            expected.Add(new Vector2(1, 1));
+            expected.Add(new Vector2(0, 0));
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
