7aa53bec3b8f23e39c4c152ed1678feb5ee47bd7
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-16 00:19:44 -0600

    REFACTOR Making passing test for different value of Right GetNeighborsFrom

--- PuzzleSolverUnitTest/DirectionSearchStrategyTests/RightDirectionSearchStrategyTest.cs
+++ PuzzleSolverUnitTest/DirectionSearchStrategyTests/RightDirectionSearchStrategyTest.cs
@@ -35,5 +35,23 @@ namespace PuzzleSolverUnitTest.DirectionSearchStrategyTests
 
             Assert.AreEqual(expected, result);
         }
+
+        [Test, TestCaseSource(typeof(DirectionSearchStrategyTestData), Base4x4PuzzleTestCase)]
+        public void Given4x4WordSearchPuzzleWhenPassing03And4ToGetNeighborsFromThenGetNeighborsFromReturnsListOfLocationsFrom03Right(WordSearchPuzzle puzzle)
+        {
+            IDirectionSearchStrategy sut = CreateInstance(puzzle);
+
+            Vector2 startLocation = new Vector2(0, 3);
+            int length = 4;
+
+            List<Vector2> result = sut.GetNeighborsFrom(startLocation, length);
+            List<Vector2> expected = new List<Vector2>();
+            expected.Add(new Vector2(0, 3));
+            expected.Add(new Vector2(1, 3));
+            expected.Add(new Vector2(2, 3));
+            expected.Add(new Vector2(3, 3));
+
+            Assert.AreEqual(expected, result);
+        }
     }
 }
