a550f32e4970670e41fdcaa519694674ae3bfaf3
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-16 00:10:29 -0600

    REFACTOR Making passing test for left GetNeighborsFrom

--- PuzzleSolverUnitTest/DirectionSearchStrategyTests/LeftDirectionSearchStrategyTest.cs
+++ PuzzleSolverUnitTest/DirectionSearchStrategyTests/LeftDirectionSearchStrategyTest.cs
@@ -4,6 +4,7 @@ using PuzzleSolverProject.GetNeighbors;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,24 @@ namespace PuzzleSolverUnitTest.DirectionSearchStrategyTests
         protected override IDirectionSearchStrategy CreateInstance(WordSearchPuzzle puzzle)
         {
             return new LeftDirectionSearchStrategy(puzzle);
+        }
+
+        [Test, TestCaseSource(typeof(DirectionSearchStrategyTestData), Base4x4PuzzleTestCase)]
+        public void Given4x4WordSearchPuzzleWhenPassing30And4ToGetNeighborsFromThenGetNeighborsFromReturnsListOfLocationsFrom30Left(WordSearchPuzzle puzzle)
+        {
+            IDirectionSearchStrategy sut = CreateInstance(puzzle);
+
+            Vector2 startLocation = new Vector2(3, 0);
+            int length = 4;
+
+            List<Vector2> result = sut.GetNeighborsFrom(startLocation, length);
+            List<Vector2> expected = new List<Vector2>();
+            expected.Add(new Vector2(3, 0));
+            expected.Add(new Vector2(2, 0));
+            expected.Add(new Vector2(1, 0));
+            expected.Add(new Vector2(0, 0));
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
