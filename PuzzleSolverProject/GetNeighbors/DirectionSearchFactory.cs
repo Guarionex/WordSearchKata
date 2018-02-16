5f7aab42b526a4e38723db4f6e8df8c9e2915d83
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-15 23:38:02 -0600

    RED Making unit test for DownDirectionSearchStrategy

--- PuzzleSolverProject/GetNeighbors/DirectionSearchFactory.cs
+++ PuzzleSolverProject/GetNeighbors/DirectionSearchFactory.cs
@@ -14,7 +14,7 @@ namespace PuzzleSolverProject.GetNeighbors
         {
             GetNeighborStrategy = new Dictionary<DirectionEnum, IDirectionSearchStrategy>();
             GetNeighborStrategy.Add(DirectionEnum.Up, new UpDirectionSearchStrategy(puzzle));
-            GetNeighborStrategy.Add(DirectionEnum.Down, new DownDirectionSearchStrategy());
+            GetNeighborStrategy.Add(DirectionEnum.Down, new DownDirectionSearchStrategy(puzzle));
             GetNeighborStrategy.Add(DirectionEnum.Left, new LeftDirectionSearchStrategy());
             GetNeighborStrategy.Add(DirectionEnum.Right, new RightDirectionSearchStrategy());
             GetNeighborStrategy.Add(DirectionEnum.UpLeft, new UpLeftDirectionSearchStrategy());
---
+++ PuzzleSolverUnitTest/DirectionSearchStrategyTests/DownDirectionSearchStrategyTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using PuzzleSolverProject;
+using PuzzleSolverProject.GetNeighbors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleSolverUnitTest.DirectionSearchStrategyTests
+{
+    [TestFixture(typeof(DownDirectionSearchStrategy))]
+    public class DownDirectionSearchStrategyTest<T> : DirectionSearchStrategyBaseTest where T : IDirectionSearchStrategy
+    {
+        protected override IDirectionSearchStrategy CreateInstance(WordSearchPuzzle puzzle)
+        {
+            return new DownDirectionSearchStrategy(puzzle);
+        }
+    }
+}
