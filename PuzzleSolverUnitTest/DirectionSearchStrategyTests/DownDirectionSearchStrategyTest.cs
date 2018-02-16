a2736019ba1bf41d9312e2bf73002c3f7d13c248
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-02-15 23:44:20 -0600

    REFACTOR Making passing test for different value of Down GetNeighborsFrom

--- PuzzleSolverUnitTest/DirectionSearchStrategyTests/DownDirectionSearchStrategyTest.cs
+++ PuzzleSolverUnitTest/DirectionSearchStrategyTests/DownDirectionSearchStrategyTest.cs
@@ -35,5 +35,23 @@ namespace PuzzleSolverUnitTest.DirectionSearchStrategyTests
 
             Assert.AreEqual(expected, result);
         }
+
+        [Test, TestCaseSource(typeof(DirectionSearchStrategyTestData), Base4x4PuzzleTestCase)]
+        public void Given4x4WordSearchPuzzleWhenPassing30And4ToGetNeighborsFromThenGetNeighborsFromReturnsListOfLocationsFrom30Down(WordSearchPuzzle puzzle)
+        {
+            IDirectionSearchStrategy sut = CreateInstance(puzzle);
+
+            Vector2 startLocation = new Vector2(3, 0);
+            int length = 4;
+
+            List<Vector2> result = sut.GetNeighborsFrom(startLocation, length);
+            List<Vector2> expected = new List<Vector2>();
+            expected.Add(new Vector2(3, 0));
+            expected.Add(new Vector2(3, 1));
+            expected.Add(new Vector2(3, 2));
+            expected.Add(new Vector2(3, 3));
+
+            Assert.AreEqual(expected, result);
+        }
     }
 }
